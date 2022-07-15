a44b117ab28aed0959a6199395fb677419625a3a
Shushanik Zargaryan <shushanik@benekiva.com>
2022-07-15 10:13:10 +0400

    Improvements

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,15 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-// ADD MONGO DB URL HERE
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r93hr.mongodb.net/?retryWrites=true&w=majority`),
     TodoModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
